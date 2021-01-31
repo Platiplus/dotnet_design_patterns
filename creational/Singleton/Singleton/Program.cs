c977ed43c7ba7bf35f450d20b752d4631994ebbb
Platiplus <quazarweb@gmail.com>
2021-01-30 23:54:01 -0300

    Implementing Singleton Pattern

---
+++ creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Press any key to get an instance of Singleton Class");
+			Console.ReadKey();
+
+			Singleton instanceOne = Singleton.GetInstance;
+			Console.ReadKey();
+			
+			Singleton instanceTwo = Singleton.GetInstance;
+			Console.ReadKey();
+			
+			Singleton instanceThree = Singleton.GetInstance;
+			Console.ReadKey();
+		}
+	}
+}
---
+++ creational/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+	public sealed class Singleton
+	{
+		private static Singleton instance = null;
+		public static Singleton GetInstance
+		{
+			get 
+			{
+				if (instance == null)
+				{
+					Console.WriteLine("New instance of Singleton Class");
+					instance = new Singleton();
+				}
+				else 
+				{
+					Console.WriteLine("Singleton class already has instance, serving old instance.");
+				}
+
+				return instance;
+			}
+		}
+	}
+}
