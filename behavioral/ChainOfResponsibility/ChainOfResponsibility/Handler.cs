6c1843960ec2b69fe1ff5ba13db3d4f59015ba9d
Platiplus <quazarweb@gmail.com>
2021-02-07 18:24:06 -0300

    Implemented ChainOfResponsibility Pattern

---
+++ behavioral/ChainOfResponsibility/ChainOfResponsibility/Handler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility
+{
+	public abstract class Handler
+	{
+		protected Handler Successor;
+
+		public void SetSuccessor(Handler successor)
+		{
+			Successor = successor;
+		}
+
+		public abstract void HandleRequest(int request);
+
+	}
+}
---
+++ behavioral/ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Handler h1 = new ConcreteHandlerOne();
+			Handler h2 = new ConcreteHandlerTwo();
+			Handler h3 = new ConcreteHandlerThree();
+
+			h1.SetSuccessor(h2);
+			h2.SetSuccessor(h3);
+
+			int[] requests = { 2, 5, 24, 22, 18, 27, 20 };
+
+			foreach (var request in requests)
+			{
+				h1.HandleRequest(request);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
