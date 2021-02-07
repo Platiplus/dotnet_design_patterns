455e5dd391d4c6c114f828e502f90b03b0363918
Platiplus <quazarweb@gmail.com>
2021-02-07 16:54:23 -0300

    Implemented Facade Pattern

---
+++ structural/Facade/Facade/Facade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Facade
+{
+	public class Facade
+	{
+		private readonly SubsystemOne One;
+		private readonly SubsystemTwo Two;
+		private readonly SubsystemThree Three;
+
+		public Facade()
+		{
+			One = new SubsystemOne();
+			Two = new SubsystemTwo();
+			Three = new SubsystemThree();
+		}
+
+		public void MethodA()
+		{
+			Console.WriteLine("\n---------- MethodA ----------");
+			One.MethodOne();
+			Three.MethodThree();
+		}
+
+		public void MethodB()
+		{
+			Console.WriteLine("\n---------- MethodB ----------");
+			Two.MethodTwo();
+			Three.MethodThree();
+		}
+	}
+}
