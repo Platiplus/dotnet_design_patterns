d1bfe12528c542057dd21bab85461f70fcd486ac
Platiplus <quazarweb@gmail.com>
2021-02-07 19:10:10 -0300

    Implemented Interpreter Pattern

---
+++ behavioral/Interpreter/Interpreter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string roman = "MCMXXVIII";
+
+			Context context = new Context(roman);
+
+			List<Expression> listExpressions = new List<Expression>();
+			listExpressions.Add(new ThousandsExpression());
+			listExpressions.Add(new HundredsExpression());
+			listExpressions.Add(new DozensExpression());
+			listExpressions.Add(new UnitsExpression());
+
+			foreach (Expression exp in listExpressions)
+			{
+				exp.Interpreter(context);
+			}
+
+			Console.WriteLine($"{roman} = {context.Output}");
+			Console.ReadKey();
+		}
+	}
+}
