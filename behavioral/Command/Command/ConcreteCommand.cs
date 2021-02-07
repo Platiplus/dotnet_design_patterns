b72ea83e9ff552f3d7441a0bd6b2aed89b35f54a
Platiplus <quazarweb@gmail.com>
2021-02-07 18:36:30 -0300

    Implemented Command Pattern

---
+++ behavioral/Command/Command/ConcreteCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Command
+{
+	public class ConcreteCommand : Command
+	{
+		public ConcreteCommand(Receiver receiver) : base(receiver) {}
+		public override void Execute()
+		{
+			Receiver.Action();
+		}
+	}
+}
---
+++ behavioral/Command/Command/Invoker.cs
@@ -0,0 +1,17 @@
+﻿namespace Command
+{
+	public class Invoker 
+	{
+		private Command Command;
+
+		public void SetCommand(Command command)
+		{
+			Command = command;
+		}
+
+		public void ExecuteCommand()
+		{
+			Command.Execute();
+		}
+	}
+}
