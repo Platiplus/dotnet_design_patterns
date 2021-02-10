c83598fc44e2ea967c505b240186eb158a019387
Platiplus <quazarweb@gmail.com>
2021-02-09 21:32:04 -0300

    Implemented Iterator Pattern

---
+++ behavioral/Iterator/Iterator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Iterator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConcreteAggregate aggregate = new ConcreteAggregate();
+			aggregate[0] = "A";
+			aggregate[1] = "B";
+			aggregate[2] = "C";
+			aggregate[3] = "D";
+
+			Iterator iterator = aggregate.CreateIterator();
+
+			Console.WriteLine("Interacting with the collection of items.");
+
+			object item = iterator.First();
+
+			while (item != null)
+			{
+				Console.WriteLine(iterator.CurrentItem());
+				item = iterator.Next();
+			}
+
+			Console.WriteLine("Printing is done = {0}", iterator.IsDone());
+			Console.ReadKey();
+		}
+	}
+}
