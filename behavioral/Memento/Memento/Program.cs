7e573fa8a8c39fffe6f79e3a8214089bc7d2ac8f
Platiplus <quazarweb@gmail.com>
2021-02-10 22:00:21 -0300

    Implemented Memento Pattern

---
+++ behavioral/Memento/Memento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Memento
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Originator originator = new Originator();
+			originator.State = "On";
+
+			Caretaker caretaker = new Caretaker();
+			caretaker.Memento = originator.CreateMemento();
+
+			originator.State = "Off";
+
+			originator.SetMemento(caretaker.Memento);
+
+			Console.ReadKey();
+		}
+	}
+}
