eb3508cf922c60a5f8b4627c9abde4d26b4a0f29
Platiplus <quazarweb@gmail.com>
2021-02-11 22:14:50 -0300

    Implemented Visitor Pattern

---
+++ behavioral/Visitor/Visitor/ConcreteVisitorOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Visitor
+{
+	public class ConcreteVisitorOne : Visitor
+	{
+		public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+		{
+			Console.WriteLine($"{concreteElementA.GetType().Name} visited by {this.GetType().Name}");
+		}
+
+		public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+		{
+			Console.WriteLine($"{concreteElementB.GetType().Name} visited by {this.GetType().Name}");
+
+		}
+	}
+}
---
+++ behavioral/Visitor/Visitor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Visitor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ObjectStructure objectStructure = new ObjectStructure();
+			objectStructure.Attach(new ConcreteElementA());
+			objectStructure.Attach(new ConcreteElementB());
+
+			ConcreteVisitorOne concreteVisitorOne = new ConcreteVisitorOne();
+			ConcreteVisitorTwo concreteVisitorTwo = new ConcreteVisitorTwo();
+
+			objectStructure.Accept(concreteVisitorOne);
+			objectStructure.Accept(concreteVisitorTwo);
+
+			Console.ReadKey();
+		}
+	}
+}
