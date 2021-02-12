bce58dd3435c9a952c21fb8f9c7d9cd0bbf76cf0
Platiplus <quazarweb@gmail.com>
2021-02-11 21:52:26 -0300

    Implemented Strategy Pattern

---
+++ behavioral/Strategy/Strategy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strategy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SortedList studentRecords = new SortedList();
+			studentRecords.Add("John");
+			studentRecords.Add("Doe");
+			studentRecords.Add("Mary");
+			studentRecords.Add("Ethan");
+
+			studentRecords.SetSortStrategy(new QuickSort());
+			studentRecords.Sort();
+
+			studentRecords.SetSortStrategy(new ShellSort());
+			studentRecords.Sort();
+
+			studentRecords.SetSortStrategy(new MergeSort());
+			studentRecords.Sort();
+
+			Console.ReadKey();
+		}
+	}
+}
